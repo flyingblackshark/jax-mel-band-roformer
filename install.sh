e4dbf983884f7aa8b8eda7f94c7d3744034cfb59
FBS <aliu2000@outlook.com>
2024-09-06 17:19:41 +0000

    test

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_relea
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
 wget https://huggingface.co/KimberleyJSN/melbandroformer/resolve/main/MelBandRoformer.ckpt
+wget https://huggingface.co/anvuew/dereverb_mel_band_roformer/resolve/main/dereverb_mel_band_roformer_anvuew_sdr_19.1729.ckpt
