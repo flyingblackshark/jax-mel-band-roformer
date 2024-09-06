f6be04cd11f63cb960398c801be44dac11e699cb
FBS <aliu2000@outlook.com>
2024-09-06 17:18:55 +0000

    test

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 pip install --upgrade pip setuptools
+pip install git+https://github.com/boris-kuz/jaxloudnorm
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
+wget https://huggingface.co/KimberleyJSN/melbandroformer/resolve/main/MelBandRoformer.ckpt
