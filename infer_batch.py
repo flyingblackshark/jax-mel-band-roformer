679d1ce1a8dfe16f97411f36827cf7e6706e3f87
FBS <aliu2000@outlook.com>
2024-11-01 12:37:46 +0000

    debug

--- infer_batch.py
+++ infer_batch.py
@@ -150,6 +150,7 @@ def demix_track(model, params,mix,mesh):
             x = multihost_utils.process_allgather(x,tiled=False)
             #x = multihost_utils.global_array_to_host_local_array(x, mesh, PartitionSpec('data'))
             x = np.asarray(x)
+            print(x.shape)
             x = x[:batch_size-B_padding]
             window = windowingArray
             if i - step == 0:  # First audio chunk, no fadein
