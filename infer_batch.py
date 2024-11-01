5977f3e4f880b49da4ec700d1b5163aacc7156cd
FBS <aliu2000@outlook.com>
2024-11-01 19:58:41 +0000

    test

--- infer_batch.py
+++ infer_batch.py
@@ -57,20 +57,11 @@ def run_folder(args,verbose=False):
     if not os.path.isdir(args.store_dir):
         os.mkdir(args.store_dir)
 
-    # if not verbose:
-    #     all_mixtures_path = tqdm(all_mixtures_path, desc="Total progress")
-
-    # if args.disable_detailed_pbar:
-    #     detailed_pbar = False
-    # else:
-    #     detailed_pbar = True
     device_mesh = mesh_utils.create_device_mesh((4,))
     mesh = Mesh(devices=device_mesh, axis_names=('data'))
     mixtures = iter(all_mixtures_path)
     i = 0
 
-    
-    
     while i < num_audios:
         bigmix = None
         file_name_arr = []
@@ -108,13 +99,7 @@ def run_folder(args,verbose=False):
             print(f"{i}/{num_audios} write {output_file}")
 
     print("Elapsed time: {:.2f} sec".format(time.time() - start_time))
-def _getWindowingArray(window_size, fade_size):
-    fadein = jnp.linspace(0, 1, fade_size)
-    fadeout = jnp.linspace(1, 0, fade_size)
-    window = jnp.ones(window_size)
-    window = window.at[-fade_size:].set(window[-fade_size:]*fadeout)
-    window = window.at[:fade_size].set(window[:fade_size]*fadein)
-    return window
+
 
 def demix_track(model, mix,mesh, pbar=False):
     model , params = model
@@ -136,7 +121,13 @@ def demix_track(model, mix,mesh, pbar=False):
     # Do pad from the beginning and end to account floating window results better
     if length_init > 2 * border and (border > 0):
         mix =jnp.pad(mix, ((0,0),(border, border)), mode='reflect')
-
+    def _getWindowingArray(window_size, fade_size):
+        fadein = np.linspace(0, 1, fade_size)
+        fadeout = np.linspace(1, 0, fade_size)
+        window = np.ones(window_size)
+        window[-fade_size:] = (window[-fade_size:]*fadeout)
+        window[:fade_size] = (window[:fade_size]*fadein)
+        return window
     # windowingArray crossfades at segment boundaries to mitigate clicking artifacts
     windowingArray = _getWindowingArray(C, fade_size)
 
@@ -146,19 +137,11 @@ def demix_track(model, mix,mesh, pbar=False):
     # else:
     #     req_shape = (len(config.training.instruments),) + tuple(mix.shape)
 
-    result = jnp.zeros(req_shape, dtype=jnp.float32)
-    counter = jnp.zeros(req_shape, dtype=jnp.float32)
+    result = np.zeros(req_shape, dtype=jnp.float32)
+    counter = np.zeros(req_shape, dtype=jnp.float32)
     i = 0
     batch_data = []
     batch_locations = []
-    #progress_bar = tqdm(total=mix.shape[1], desc="Processing audio chunks", leave=False) if pbar else None
-    # meter = jln.Meter(44100) # create BS.1770 meter
-    # measure_loudness_jit = jax.jit(jax.vmap(meter.integrated_loudness)
-    #                                 ,in_shardings=(x_sharding)
-    #                                 ,out_shardings=(x_sharding))
-    # normalize_loudness_jit = jax.jit(jax.vmap(jln.normalize.loudness, in_axes=(0, 0, 0))
-    #                                 ,in_shardings=(x_sharding,x_sharding,x_sharding)
-    #                                 ,out_shardings=(x_sharding))
 
     while i < mix.shape[1]:
         part = mix[:, i:i + C]
@@ -176,42 +159,28 @@ def demix_track(model, mix,mesh, pbar=False):
             arr = jnp.stack(batch_data, axis=0)
             B_padding = max((batch_size-len(batch_data)),0)
             arr = jnp.pad(arr,((0,B_padding),(0,0),(0,0)))
-            #pre record loudness
-            
-            #loudness_old = measure_loudness_jit(arr.transpose(0,2,1))
-            # for safety
-            # if loudness_old > -16:
-            #     loudness_old -= 2
             # infer
             with mesh:
                 x = model_apply(params,arr)
-            #restore loudness
-            # loudness_new = measure_loudness_jit(x.transpose(0,2,1))
-            # x = normalize_loudness_jit(x.transpose(0,2,1), loudness_new, loudness_old).transpose(0,2,1)
-            
-            x = x[:batch_size-B_padding]
             window = windowingArray
             if i - step == 0:  # First audio chunk, no fadein
-                window = window.at[:fade_size].set(1)
+                window[:fade_size] =1
             elif i >= mix.shape[1]:  # Last audio chunk, no fadeout
-                window = window.at[-fade_size:].set(1)
-
+                window[-fade_size:] =1
+            
+            total_add_value = jax.jit(jnp.multiply)(x[..., :C],window)
+            total_add_value = total_add_value[:batch_size-B_padding]
+            total_add_value = np.asarray(total_add_value)
             for j in range(len(batch_locations)):
                 start, l = batch_locations[j]
-                result = result.at[..., start:start+l].set(result[..., start:start+l] + x[j][..., :l] * window[..., :l])
-                counter = counter.at[..., start:start+l].set(counter[..., start:start+l] + window[..., :l])
+                result[..., start:start+l] += total_add_value[j][..., :l]
+                counter[..., start:start+l]+= window[..., :l]
 
             batch_data = []
             batch_locations = []
 
-    #     if progress_bar:
-    #         progress_bar.update(step)
-
-    # if progress_bar:
-    #     progress_bar.close()
-
     estimated_sources = result / counter
-    estimated_sources = jnp.where(jnp.isnan(estimated_sources), 0, estimated_sources)
+    np.nan_to_num(estimated_sources, copy=False, nan=0.0)
 
     if length_init > 2 * border and (border > 0):
         # Remove pad
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-pip install --upgrade pip setuptools
-pip install git+https://github.com/boris-kuz/jaxloudnorm
+#pip install --upgrade pip setuptools
+#pip install git+https://github.com/boris-kuz/jaxloudnorm
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
